64de086871e54c452ef60b2ffa42d3cbfe70e5fc
wjxor <shko940575@gmail.com>
2022-12-13 02:00:05 +0900

    첫번째 버튼 누르면 다음 화면으로 전환

--- app/src/main/java/com/sbs/wjxor/be/bit_project/MainActivity.java
+++ app/src/main/java/com/sbs/wjxor/be/bit_project/MainActivity.java
@@ -2,13 +2,27 @@ package com.sbs.wjxor.be.bit_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNext = (Button) findViewById(R.id.btnNext);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), NextActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/sbs/wjxor/be/bit_project/NextActivity.java
@@ -0,0 +1,15 @@
+package com.sbs.wjxor.be.bit_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NextActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
